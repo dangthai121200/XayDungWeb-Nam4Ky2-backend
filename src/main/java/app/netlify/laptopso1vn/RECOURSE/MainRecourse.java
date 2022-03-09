c87d32ff0a9d96ac1a4e0b7ccf3445bfa564cab0
dangthai121200 <dangthai121200@gmail.com>
2022-03-09 17:37:56 +0700

    them api main

--- src/main/java/app/netlify/laptopso1vn/RECOURSE/MainRecourse.java
+++ src/main/java/app/netlify/laptopso1vn/RECOURSE/MainRecourse.java
@@ -1,13 +1,17 @@
 package app.netlify.laptopso1vn.RECOURSE;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
 public class MainRecourse {
+	
+	@GET
 	public String main() {
 		return "Chào mừng bạn đến với laptopso1vn";
 	}
+	
 }
